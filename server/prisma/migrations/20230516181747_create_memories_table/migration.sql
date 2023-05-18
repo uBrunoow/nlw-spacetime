c9b115fba26d5a3877acebd6b4b1ec361ae7a09f
Bruno Werner <101356770+uBrunoow@users.noreply.github.com>
2023-05-17 21:04:47 -0300

    [aula 3]

--- server/prisma/migrations/20230516181747_create_memories_table/migration.sql
+++ server/prisma/migrations/20230516181747_create_memories_table/migration.sql
@@ -9,11 +9,11 @@
 -- CreateTable
 CREATE TABLE "Memory" (
     "id" TEXT NOT NULL PRIMARY KEY,
+    "userId" TEXT NOT NULL,
     "coverUrl" TEXT NOT NULL,
     "content" TEXT NOT NULL,
     "isPublic" BOOLEAN NOT NULL DEFAULT false,
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "userId" TEXT NOT NULL,
     CONSTRAINT "Memory_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
@@ -21,12 +21,12 @@ CREATE TABLE "Memory" (
 PRAGMA foreign_keys=OFF;
 CREATE TABLE "new_User" (
     "id" TEXT NOT NULL PRIMARY KEY,
-    "githubId" INTEGER NOT NULL,
-    "nome" TEXT NOT NULL,
+    "githubId" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
     "login" TEXT NOT NULL,
     "avatarUrl" TEXT NOT NULL
 );
-INSERT INTO "new_User" ("id", "nome") SELECT "id", "nome" FROM "User";
+INSERT INTO "new_User" ("id", "name") SELECT "id", "name" FROM "User";
 DROP TABLE "User";
 ALTER TABLE "new_User" RENAME TO "User";
 CREATE UNIQUE INDEX "User_githubId_key" ON "User"("githubId");
