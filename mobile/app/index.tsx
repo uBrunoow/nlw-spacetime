17cd66aa173e3b967cabf2276d3bb66b3c87f002
Bruno Werner <101356770+uBrunoow@users.noreply.github.com>
2023-05-18 22:13:35 -0300

    [AULA 4]

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,25 +1,13 @@
-import { StatusBar } from "expo-status-bar";
-import { ImageBackground, Text, TouchableOpacity, View } from "react-native";
+import { Text, TouchableOpacity, View } from "react-native";
 import * as SecureStore from "expo-secure-store";
 import { useRouter } from "expo-router";
-
-import {
-  useFonts,
-  Roboto_400Regular,
-  Roboto_700Bold,
-} from "@expo-google-fonts/roboto";
-
-import { BaiJamjuree_700Bold } from "@expo-google-fonts/bai-jamjuree";
-
-import blurBg from "../src/assets/bg-blur.png";
-import Stripes from "../src/assets/bg-stripes.svg";
 import NLWLogo from "../src/assets/nlw-spacetime-logo.svg";
-import { styled } from "nativewind";
 import { makeRedirectUri, useAuthRequest } from "expo-auth-session";
 import { useEffect } from "react";
 import { api } from "../src/lib/api";
-
-const StyledStripes = styled(Stripes);
+import { BaiJamjuree_700Bold } from "@expo-google-fonts/bai-jamjuree";
+import { Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
+import { useFonts } from "expo-font";
 
 const discovery = {
   authorizationEndpoint: "https://github.com/login/oauth/authorize",
@@ -54,9 +42,6 @@ export default function App() {
     BaiJamjuree_700Bold,
   });
 
-  if (!hasLoadedFonts) {
-    return null;
-  }
 
   async function handleGithubOAuthCode(code: string) {
     const response = await api.post("/register", {
@@ -78,13 +63,7 @@ export default function App() {
   }, [response]);
 
   return (
-    <ImageBackground
-      source={blurBg}
-      className="relative flex-1 items-center bg-gray-900 px-8 py-10"
-      imageStyle={{ position: "absolute", left: "-100%" }}
-    >
-      <StyledStripes className="absolute left-2" />
-
+    <View className="flex-1 items-center px-8 py-10">
       <View className="flex-1 items-center justify-center gap-6 ">
         <NLWLogo />
         <View className="space-y-2">
@@ -112,7 +91,6 @@ export default function App() {
       </Text>
 
       <Text className="Text-gray-50 text-4xl font-bold">Hello World</Text>
-      <StatusBar style="light" translucent />
-    </ImageBackground>
+    </View>
   );
 }
---
+++ mobile/app/new.tsx
@@ -0,0 +1,64 @@
+import { ScrollView, Switch, Text, Text, TextInput, TouchableOpacity, View } from "react-native";
+import NLWLogo from "../src/assets/nlw-spacetime-logo.svg";
+import { Link } from "expo-router";
+import Icon from "@expo/vector-icons/Feather"
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import React, { useState } from "react";
+
+export default function NewMemory() {
+
+  const { bottom, top } = useSafeAreaInsets()
+  const [isPublic, setIsPublic] = useState(false)
+
+  return (
+    <ScrollView className="flex-1 px-8" contentContainerStyle={{paddingBottom: bottom, paddingTop: top}}>
+      <View className="mt-4 flex-row items-center justify-between">
+        <NLWLogo/>
+
+        <Link href="/memories" asChild>
+          <TouchableOpacity className="h-10 w-10 items-center justify-center rounded-full bg-purple-500 ">
+            <Icon name="arrow-left" size={16} color="#fff"/>
+          </TouchableOpacity>
+        </Link>
+      </View>
+
+      <View className="space-y-6 mt-6">
+        <View className="flex-row items-center gap-2">
+          <Switch 
+          value={isPublic} 
+          onValueChange={setIsPublic}
+          thumbColor={isPublic ? '#9b79ea' : '#9e9ea0'}
+          trackColor={{
+            false: "#767577",
+            true: "#372560"
+          }}
+          />
+          <Text className="font-body text-base text-gray-200">
+            Tornar memória pública
+          </Text>
+        </View>
+        <TouchableOpacity activeOpacity={0.7} className="h-32 items-center justify-center rounded-large border border-dashed border-gray-500 bg-black/20">
+          <View className=" felx-row  items-center gap-2" >
+            <Icon name="image" color="#fff" />
+            <Text className="font-body text-small text-gray-200">Adicionar foto ou vídeo de capa</Text>
+          </View>
+        </TouchableOpacity>
+        <TextInput
+          multiline
+          className="p-0 font-body text-large text-gray-50 "
+          placeholderTextColor="#56565a"
+          placeholder="Fique livre para adicionar fotos, vídeos e relatos sobre essa experiência que você quer lembrar para sempre."
+        />
+
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className="mt-6 rounded-full items-center self-end bg-green-500 px-5 py-2"
+        >
+          <Text className="font-alt text-sm uppercase text-black">
+            Salvar
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </ScrollView>
+  );
+}
